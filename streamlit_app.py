da7f708b884238926ddf3796914c03bcb5cd1b2d
IlariaPiccolo <155577325+IlariaPiccolo@users.noreply.github.com>
2025-03-07 11:47:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,9 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-    
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
     
     #st.write (ingredients_string)
     #st.write(my_insert_stmt)
