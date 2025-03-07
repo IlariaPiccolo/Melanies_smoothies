322e790244244f55228913a6985de6754b2028b2
IlariaPiccolo <155577325+IlariaPiccolo@users.noreply.github.com>
2025-03-07 11:24:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,59 +1,119 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requestes
-
+import requests
+ 
 helpful_links = [
     "https://docs.streamlit.io",
     "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
     "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
     "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
 ]
-
+ 
 # Write directly to the app
-st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
+st.title("Example Streamlit App :balloon:")
 st.write(
-    f"""Choose the fruit you want in your custom smoothie!!
+    f"""Replace the code in this example app with your own code! And if you're new to Streamlit, here are some helpful links:
+ 
+    • :page_with_curl: [Streamlit open source documentation]({helpful_links[0]})
+    • :snow: [Streamlit in Snowflake documentation]({helpful_links[1]}) 
+    • :books: [Demo repo with templates]({helpful_links[2]})
+    • :memo: [Streamlit in Snowflake release notes]({helpful_links[3]})
     """
 )
-
-
-#option = st.selectbox(
-#    "What's your favourite fruit?",
-#    ("Banana", "Strawberries", "Peaches"),
-#)
-
-
-
+ 
+ 
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The Name on your Smoothie will be:', name_on_order)
+ 
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
-
-
+st.dataframe(data=my_dataframe, use_container_width=True)
+ 
+ 
 ingredients_list = st.multiselect(
-    'choose up to 5 ingredients:',
-    my_dataframe
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
 )
-
-
+ 
 if ingredients_list:
-    ingredients_string = ''
-
-
+    st.write(ingredients_list)
+    st.text(ingredients_list)
+    ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    #st.write(ingredients_string)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
-
+ 
+    st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
-
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-    
-    
-    #st.write (ingredients_string)
-    #st.write(my_insert_stmt)
+ 
+ # Import python packages
+import streamlit as st
+from snowflake.snowpark.functions import col
+import requests
+ 
+helpful_links = [
+    "https://docs.streamlit.io",
+    "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
+    "https://github.com/Snowflake-Labs/snowflake-demo-streamlit",
+    "https://docs.snowflake.com/en/release-notes/streamlit-in-snowflake"
+]
+ 
+# Write directly to the app
+st.title("Example Streamlit App :balloon:")
+st.write(
+    f"""Replace the code in this example app with your own code! And if you're new to Streamlit, here are some helpful links:
+ 
+    • :page_with_curl: [Streamlit open source documentation]({helpful_links[0]})
+    • :snow: [Streamlit in Snowflake documentation]({helpful_links[1]}) 
+    • :books: [Demo repo with templates]({helpful_links[2]})
+    • :memo: [Streamlit in Snowflake release notes]({helpful_links[3]})
+    """
+)
+ 
+ 
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The Name on your Smoothie will be:', name_on_order)
+ 
+cnx = st.connection("snowflake")
+session = cnx.session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+ 
+ 
+ingredients_list = st.multiselect(
+    'Choose up to 5 ingredients:'
+    , my_dataframe
+    , max_selections=5
+)
+ 
+if ingredients_list:
+    st.write(ingredients_list)
+    st.text(ingredients_list)
+    ingredients_string=''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    #st.write(ingredients_string)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+
+ 
+    st.write(my_insert_stmt)
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered!', icon="✅")
+ 
+ 
